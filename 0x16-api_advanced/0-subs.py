8e42240f9680d15346e60783a2c37b186c153e66
Otonda1 <charlesotonda@gmail.com>
2023-11-08 01:19:45 +0300

    adjustment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': (
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                'Mozilla/5.0 (Windows NT 10.0; Win32; x32) AppleWebKit/537.36'
                 '(KHTML, like Gecko) '
                 'Chrome/100.0.0.0 Safari/537.36'
                 ),
